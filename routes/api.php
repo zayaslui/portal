fb2b515755be69ae45bfaee8f8073f67c1d80d44
zayaslui <zayaslui@gmail.com>
2022-11-01 11:24:55 -0300

    update

--- routes/api.php
+++ routes/api.php
@@ -7,13 +7,10 @@ use App\Models\Post;
 
 /*REST CONTROLLER*/
 
-Route::get('/obtenerPosts', [App\Http\Controllers\PostController::class, 'obtenerPosts']);
-Route::get('/test', [App\Http\Controllers\PostController::class, 'test']);
-
 Route::group(['prefix' => 'portal'], function () {
 
-    Route::post('/login', [App\Http\Controllers\PortalController::class     ,'login']);
-    Route::post('/register', [App\Http\Controllers\PortalController::class  ,'register']);
+    Route::post('/login'    , [App\Http\Controllers\PortalController::class     ,'login']);
+    Route::post('/register' , [App\Http\Controllers\PortalController::class     ,'register']);
 
 });
 
